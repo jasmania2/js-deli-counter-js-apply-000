73ef981f7eb51c42c68a5b0d78f0ae615924b092
sensible-draft-9261 <jasmine.williamson@me.com>
2020-02-25 22:50:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,10 +15,15 @@ function nowServing(katzDeliLine){
 }
 
 function currentLine(katzDeliLine){
+  
   for (let x=0;x<katzDeliLine.length;x++){
     var array=[];
     
     array.push((x+1) +". " + katzDeliLine[x]);
   }
-  return "The current line is: " + array;
+if (katzDeliLine.length>0){
+    return "The line is currently: " + array;
+  } else {
+    return "The line is currently empty.";
+  }
 }
