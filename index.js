ec575332226e828c92c675b3234d5d0f2395c7ef
sensible-draft-9261 <jasmine.williamson@me.com>
2020-02-25 22:55:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function currentLine(katzDeliLine){
    var array=[];
   for (let x=0;x<katzDeliLine.length;x++){
    
-    array.push((x+1) +". " + katzDeliLine[x]+ " ");
+    array.push((x+1) +". " + katzDeliLine[x]);
   }
 if (katzDeliLine.length>0){
     return "The line is currently: " + array;
