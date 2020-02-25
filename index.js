6bdff26620d90c606e62dce0b90470202bce7476
sensible-draft-9261 <jasmine.williamson@me.com>
2020-02-25 21:40:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function currentLine(katzDeliLine){
   for (let x=0;x<katzDeliLine.length;x++){
     let array=[];
   if (katzDeliLine[0] !== undefined){
-    var list = katzDeliLine.length +". " + katzDeliLine[x];
+    var list = katzDeliLine[x].length +". " + katzDeliLine[x];
     array.push(list);
     return "The line is currently: " + array;
   } else {
